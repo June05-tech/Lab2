773796614996b5e6e35707158ee0827b7f7c9068
Junhyeok <Jang@snu.ac.kr>
2024-06-13 02:04:04 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -3,20 +3,31 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
 
+	if (cnt == M) {
+		for (auto m : vec)
+			cout << m << " ";
+		cout << endl;
+		return;
+	}
+	
+	for (int i = 1; i <= N; i++) {
 
-
-
-
-
+		if (vec.empty() || (!vec.empty() && i > vec[vec.size() - 1])) {
+			vec.push_back(i);
+			func(++cnt);
+			vec.erase(vec.end() - 1);
+			cnt--;
+		}
+	}
 
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+	cin >> N >> M;
+	func(0);
 }
